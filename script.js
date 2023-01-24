1717cb1ff3c02f6771a8dfdb5ba82cbb4393ca7d
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-01-24 16:40:56 +0100

    copy files

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+let intro = document.querySelector('.intro')
+let logo = document.querySelector('.logo-header')
+let logoSpan = document.querySelectorAll('.logo')
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.remove('active');
+                span.classList.add('fade');
+            }, (idx + 1) * 50)
+        });
+    },2000)
+
+    setTimeout(() => {
+        intro.style.top = '-100vh';
+    },2300)
+
+    })  
+});
+
+// window pop up
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        3800
+    )
+  });
+  document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+  });
 
