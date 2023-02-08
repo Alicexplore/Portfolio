bc3a5cf7b17fc74f3bc31393714fcfce627cdcb4
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-02-08 21:09:47 +0100

    trying GSAP

--- scriptshape.js
+++ scriptshape.js
@@ -1,36 +1,65 @@
-// GSAP Animation
-const shape = document.querySelector("#shape");
-const innerShape = shape.querySelector(".inner-shape");
+// GSAP Animation 1
+// const shape = document.querySelector("#shape");
 
-gsap.to(shape, {
-  duration: 10,
-  rotation: 360,
-  repeat: -1,
-  ease: "linear"
-});
+// gsap.to(shape, {
+//   duration: 10,
+//   rotation: 360,
+//   repeat: -1,
+//   ease: "linear"
+// });
 
-gsap.to(innerShape, {
-  duration: 10,
-  y: -100,
-  repeat: -1,
-  yoyo: true,
-  ease: "sine.inOut"
-});
+// shape.addEventListener("click", function() {
+//     gsap.to(shape, {
+//       duration: 2,
+//       opacity: 0,
+//       ease: "linear",
+//       onComplete: function() {
+//         shape.style.display = "none";
+//       }
+//     });
+//   });
 
-// Glitch Effect on click
-shape.addEventListener("click", function() {
-  gsap.to(innerShape, {
-    duration: 0.2,
-    x: "-=20",
-    repeat: 5,
-    yoyo: true,
-    ease: "sine.inOut"
-  });
-  gsap.to(shape, {
-    duration: 0.5,
-    opacity: 0,
-    onComplete: function() {
-      shape.style.display = "none";
-    }
-  });
-});
+  // GSAP Animation 2
+// const shape_2 = document.querySelector("#shape_2");
+
+// gsap.to(shape_2, {
+//   duration: 10,
+//   rotation: 360,
+//   repeat: -1,
+//   ease: "linear"
+// });
+
+// shape.addEventListener("click", function() {
+//     gsap.to(shape_2, {
+//       duration: 2,
+//       opacity: 0,
+//       ease: "linear",
+//       onComplete: function() {
+//         shape_2.style.display = "none";
+//       }
+//     });
+//   });
+
+
+    // GSAP Animation 3
+// const shape_3 = document.querySelector("#shape_3");
+
+// gsap.to(shape_3, {
+//   duration: 10,
+//   rotation: 360,
+//   repeat: -1,
+//   ease: "linear"
+// });
+
+// shape.addEventListener("click", function() {
+//     gsap.to(shape_3, {
+//       duration: 2,
+//       opacity: 0,
+//       ease: "linear",
+//       onComplete: function() {
+//         shape_3.style.display = "none";
+//       }
+//     });
+//   });
+
+
