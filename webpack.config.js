43f9cf32889ad3042b1d9ad154f4e84f30602750
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-04-17 14:12:41 +0200

    fix babel

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+      ],
+    },
+    devServer: {
+      contentBase: './dist',
+      port: 3000,
+    },
+  };
+  
