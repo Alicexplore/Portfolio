b126ed1b59259efcad42391312edb5ba28b24b17
alicexplore <alicebergonhe@gmail.com>
2023-07-26 22:43:57 +0200

    delete notes

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -28,17 +28,6 @@ const Hero = () => {
                         <Link to='contact' activeClass='active' smooth={true} spy={true} ><button className='mx-8 my-4 font-inter dark:text-[#222222] dark:bg-[#f2f2f2] text-[#f2f2f2] px-4 py-2 text lg:text-md rounded-sm 
                                 border border-[#2e2e2d] dark:border-[#f2f2f2] dark:hover:text-[#3b3b3b] bg-[#2e2e2d] hover:text-[#d4d4d4] 
                               hover:bg-[#2e2e2d] duration-500 cursor-pointer'>Contact Me</button></Link>            
-                        {/* <div className='flex items-center text-[1.2rem]'>
-                            <Link to='contact' activeClass='active' smooth={true} spy={true} ><button className='flex mx-7 lg:mx-8 my-4 font-inter dark:text-[#222222] dark:bg-[#f2f2f2] text-[#f2f2f2]  px-4 py-2 text-md rounded-sm 
-                            border border-[#222222] dark:border-[#f2f2f2] dark:hover:text-[#3b3b3b] bg-[#222222] hover:text-[#d4d4d4] 
-                          hover:bg-[#222222] duration-1000 cursor-pointer'>CONTACT</button></Link>
-                            <a href='https://www.linkedin.com/in/alicebergonhe/' target='blank' className='flex -mx-2 cursor-pointer'><button className='flex lg:my-4 font-inter dark:text-[#222222] dark:bg-[#f2f2f2] text-[#f2f2f2]  px-4 py-2 text-md rounded-sm 
-                            border border-[#222222] dark:border-[#f2f2f2] dark:hover:text-[#3b3b3b] bg-[#222222] hover:text-[#d4d4d4] 
-                          hover:bg-[#222222] duration-1000 cursor-pointer'>LINKEDIN</button></a>
-                            <a href='https://github.com/Alicexplore' target='blank' className='flex mx-5 cursor-pointer'><button className='flex lg:mx-3 my-4 font-inter dark:text-[#222222] dark:bg-[#f2f2f2] text-[#f2f2f2]  px-4 py-2 text-md rounded-sm 
-                            border border-[#222222] dark:border-[#f2f2f2] dark:hover:text-[#3b3b3b] bg-[#222222] hover:text-[#d4d4d4] 
-                          hover:bg-[#222222] duration-1000 cursor-pointer'>GITHUB</button></a>
-                        </div> */}
                     </motion.div>
                 </div>
                 <div>
