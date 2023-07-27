0d5b54a8d83069af63fdc6fc2eab90c4bebcb7d3
alicexplore <alicebergonhe@gmail.com>
2023-07-27 11:29:30 +0200

    mouse effect fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { motion } from 'framer-motion'
 import { fadeIn } from '../variants'
