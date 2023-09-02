9641308c813e60992ebb9200d26c486ccdbfb5dd
alicexplore <alicebergonhe@gmail.com>
2023-09-02 15:24:05 +0200

    fix contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -29,7 +29,7 @@ export default function Contact() {
                         to contact me. I am always happy to meet new people and discuss topics related to computer science and
                         web development, I am also open to receiving advice from experienced professionals in the field.
                     </p>
-                    <div className='grid grid-rows-1 sm:grid-cols-3 sm:'>
+                    {/* <div className='grid grid-rows-1 sm:grid-cols-3 sm:'>
                         <Link to="mailto:alicebergonhe@gmail.com" target="blank" className='cursor-pointer' ><button className='my-4 font-inter dark:text-[#222222] dark:bg-[#f2f2f2] text-[#f2f2f2] px-4 py-2 text-lg rounded-sm 
                         border border-[#2e2e2d] dark:border-[#f2f2f2] dark:hover:text-[#3b3b3b] bg-[#2e2e2d] hover:text-[#d4d4d4] 
                         hover:bg-[#222222] duration-500 cursor-pointer flex items-center justify-center'>Email<span className='flex text-xl justify-center items-center ml-3'><ion-icon name="mail-unread-outline"></ion-icon></span></button></Link> 
@@ -41,7 +41,7 @@ export default function Contact() {
                         <Link to="https://www.linkedin.com/in/alicebergonhe/" target="blank" className='cursor-pointer' ><button className='my-4 font-inter dark:text-[#222222] dark:bg-[#f2f2f2] text-[#f2f2f2] px-4 py-2 text-lg rounded-sm 
                         border border-[#2e2e2d] dark:border-[#f2f2f2] dark:hover:text-[#3b3b3b] bg-[#2e2e2d] hover:text-[#d4d4d4] 
                         hover:bg-[#222222] duration-500 cursor-pointer flex items-center justify-center'>Linkedin<span className='flex text-xl justify-center items-center ml-3'><ion-icon name="logo-linkedin"></ion-icon></span></button></Link>      
-                    </div>
+                    </div> */}
                 </motion.div>
             </div>
             <div className='grid grid-cols-1 place-items-center'>
@@ -50,7 +50,14 @@ export default function Contact() {
                     initial='hidden'
                     whileInView={'show'}
                     viewport={{ once: false, amount: 0}}>
-                    <img src="./img/linkedinlogoplus.png" alt="logo plus contact" width={350}/>
+                    <div className='grid grid-cols-2'>
+                        <div className='grid-rows-2'>
+                        <Link to="https://github.com/Alicexplore/The_Sneakers_Project" target="blank" ><button className='my-4 font-inter dark:text-[#222222] dark:bg-[#f2f2f2] text-[#f2f2f2] px-12 py-12 text-lg rounded-sm 
+                            border border-[#2e2e2d] dark:border-[#f2f2f2] dark:hover:text-[#3b3b3b] bg-[#2e2e2d] hover:text-[#d4d4d4] 
+                            hover:bg-[#222222] duration-500 cursor-pointer flex items-center justify-center'><span className=''><ion-icon name="logo-github"></ion-icon></span></button></Link> 
+                        </div>
+                    </div>
+                    {/* <img src="./img/linkedinlogoplus.png" alt="logo plus contact" width={350}/> */}
                 </motion.div>
             </div>
         </div>
